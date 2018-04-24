4e7739a1d7ebde63944320f68aa086a6031334da
Gstepanoff <gleb.stepanov@phystech.edu>
2018-04-24 10:30:37 +0300

    updates config

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx start
-sudo /etc/init.d/mysql start
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
