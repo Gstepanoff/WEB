04cc522542885e9f8120e05084b83b10fd358333
Gstepanoff <gleb.stepanov@phystech.edu>
2018-04-29 00:47:40 +0300

    Task3.1.2

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
-#! /usr/bin/python
-#
-def myapp(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type','text/plain')]
-    start_response(status, response_headers)
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+from cgi import parse_qs
+import string
+
+def app(environ, start_response):
+	body = ''
+        if environ['REQUEST_METHOD'] == 'GET':
+                a = string.replace(environ['QUERY_STRING'], '&', '\n')
+                print(a)
+		body = a
+                print(body)
+                status = '200 OK'
+                headers = [('Content-Type', 'text/plain')]
+                start_response(status, headers )
+                return [ body ]
